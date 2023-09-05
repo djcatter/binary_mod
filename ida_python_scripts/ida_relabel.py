20a06472b39bbc0cfdc9081a5271a0d4fd677ad8
djcatter <derek@catterfeld.com>
2023-09-04 18:26:49 -0600

    Corrected relabel bug

--- ida_python_scripts/ida_relabel.py
+++ ida_python_scripts/ida_relabel.py
@@ -10,8 +10,14 @@ def get_address_from_name(name):
     return hex(ea).upper()
 
 
-# Define a regular expression pattern for matching $IDA_ADDR(Name)
-pattern = r'\$IDA_ADDR\((\w+)\)'
+class LabelNotFoundException(Exception):
+    pass
+
+class UnknownOperatorException(Exception):
+    pass
+
+# Define a regular expression pattern for matching $IDA_ADDR(Name), $IDA_ADDR(Name+N), and $IDA_ADDR(Name-N)
+pattern = r'\$IDA_ADDR\((\w+)([+-]\d+)?\)'
 
 
 def ida_address_findreplace(input_filename, output_filename):
@@ -28,9 +34,26 @@ def ida_address_findreplace(input_filename, output_filename):
                 matches = re.findall(pattern, line)
 
                 # Loop over each match and replace it with the corresponding hex address from IDA
-                for name in matches:
+                for name,offset in matches:
                     hex_addr = get_address_from_name(name)
-                    line = line.replace(f'$IDA_ADDR({name})', hex_addr)
+                    if(hex_addr is None):
+                        raise LabelNotFoundException("Label '{}' not found".format(name))
+                    # If there is an offset, add it to the hex address
+                    if offset:
+                        operator = offset[0]  # Get the operator (+ or -)
+                        value = int(offset[1:])  # Get the numerical value
+                        # print("Label '{}' found".format(name))
+                        # print("Offset '{}'".format(value))
+                        # print("Operator '{}'".format(operator))
+                        if operator == '+':
+                            hex_addr = hex(int(hex_addr, 16) + value)
+                        elif operator == '-':
+                            hex_addr = hex(int(hex_addr, 16) - value)
+                        else:
+                            raise UnknownOperatorException("Operator found when looking for '{}' not found".format(name))         
+                        line = line.replace(f'$IDA_ADDR({name}{operator}{value})', hex_addr)
+                    else:                 
+                        line = line.replace(f'$IDA_ADDR({name})', hex_addr)
 
                 # Write the modified line to the output file
                 outfile.write(line)
